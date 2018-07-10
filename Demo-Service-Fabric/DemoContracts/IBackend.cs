f79753825e8b668164447bb823e0cc4ba0596f02
Daniel Iercan <diercan@isoftwaresystems.com>
2018-07-10 22:01:17 +0300

    SF Test

---
+++ Demo-Service-Fabric/DemoContracts/IBackend.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using System;
+using System.Threading.Tasks;
+
+namespace DemoContracts
+{
+    public interface IBackend:IService
+    {
+        Task<long> GetCount();
+    }
+}
